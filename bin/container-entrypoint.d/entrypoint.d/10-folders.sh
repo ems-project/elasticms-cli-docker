a6d32bffd60a881fb024723b308669e9c9642b5d
Sébastian Molle <sebastian.molle@gmail.com>
2025-05-21 15:47:35 +0200

    Feat/cronjob readonly container (#26)
    * Allow running jobs with tracking
    
    * Fix testsuite for new entrypoints
    
    * chore(dockerfile): use syntax 1.15 to enable advanced BuildKit features
    
    * FromAsCasing: 'as' and 'FROM' keywords' casing do not match
    
    * Fix: UndefinedVar: Usage of undefined variable '$PHP_EXT_SQLSRV_VERSION'
    
    * Allow running in readonly container
    
    ---------
    
    Co-authored-by: sebastian.molle@smals.be <sebastian.molle@smals.be>

---
+++ bin/container-entrypoint.d/entrypoint.d/10-folders.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+export APP_BIN_DIR="/app/sbin"
+export APP_SRC_DIR="/app/src/elasticms"
+export APP_TMP_DIR="${TMPDIR}"
+
+export APP_CONFIG_DIR="${APP_TMP_DIR}/elasticms.d"
+export APP_LOG_DIR="/app/var/log/elasticms"
+
+APP_CACHE_DIR_DEFAULT="/app/var/cache/elasticms"
+export APP_CACHE_DIR=${APP_CACHE_DIR:-"${APP_CACHE_DIR_DEFAULT}"}
+
+true
