0e8eba3502d7e410c4a056537704541e643c37d7
Sébastian Molle <sebastian.molle@gmail.com>
2025-05-21 17:13:49 +0200

    chore: refactor scripts sequence

--- bin/container-entrypoint.d/elasticms.d/20-supervisor.sh
+++ bin/container-entrypoint.d/elasticms.d/20-supervisor.sh
@@ -2,7 +2,7 @@
 
 echo "    - Configure Supervisord ..."
 
-OUTDIR="/app/etc"
+OUTDIR="/app/etc /app/var/run /app/var/log"
 mkdir -p $OUTDIR
 
 apply-template /app/config/supervisord.conf.tmpl /app/etc/supervisord.conf
